2b65c64c4ed695f29d7375a872b905d23a22cc04
HuangJian <huangjian@minieye.cc>
2023-06-13 10:51:31 +0800

    add base64 decode

--- src/encoding/base64.rs
+++ src/encoding/base64.rs
@@ -29,6 +29,48 @@ pub const DECODE_STD_MAP: &[u8] = &[
     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
 ];
 
+#[derive(Debug, PartialEq)]
+pub enum Base64Error {
+    InvalidByte(InvalidByteError),
+    InvalidLength,
+}
+
+impl std::fmt::Display for Base64Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidByte(e) => e.fmt(f),
+            Self::InvalidLength => write!(f, "encoding/base64: invalid input length"),
+        }
+    }
+}
+
+impl std::error::Error for Base64Error {}
+
+impl From<InvalidByteError> for Base64Error {
+    fn from(e: InvalidByteError) -> Self {
+        Self::InvalidByte(e)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct InvalidByteError {
+    b: u8,
+}
+
+impl InvalidByteError {
+    pub fn new(b: u8) -> Self {
+        Self { b }
+    }
+}
+
+impl std::fmt::Display for InvalidByteError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "encoding/base64: invalid byte: {}", self.b)
+    }
+}
+
+impl std::error::Error for InvalidByteError {}
+
 /// Calculate base64 encoded string length
 ///
 /// - @param src: raw string
@@ -45,6 +87,12 @@ pub fn encode_str_len(src: &str) -> usize {
 #[inline]
 pub fn encode_len(src_len: usize) -> usize {
     (src_len + 2) / 3 * 4
+}
+
+/// Calculate base64 decoded data length. Padded base64 should always be
+/// a multiple of 4 bytes in length.
+pub fn decode_len(n: usize) -> usize {
+    n / 4 * 3
 }
 
 /// Encode bytes to base64 bytes
@@ -148,6 +196,11 @@ pub fn encode_bytes(src: &[u8]) -> Vec<u8> {
     encode_bytes_with_map(src, ENCODE_STD)
 }
 
+pub fn encode_to_string(src: &[u8]) -> String {
+    let dst = encode_bytes(src);
+    String::from_utf8(dst).unwrap()
+}
+
 /// Encode string to base64 string
 ///
 /// - @param src: raw string
@@ -178,6 +231,76 @@ pub fn decode_map(encode_map: &[u8]) -> Vec<u8> {
         })
 }
 
+#[inline]
+fn from_char(b: u8, decode_map: &[u8]) -> Result<u8, InvalidByteError> {
+    let out = decode_map[b as usize];
+    if out == 0xFF {
+        return Err(InvalidByteError::new(b));
+    }
+    return Ok(out);
+}
+
+pub fn decode(dst: &mut [u8], src: &[u8], decode_map: &[u8]) -> Result<usize, Base64Error> {
+    let src_len = src.len();
+    if src_len == 0 || src_len % 4 != 0 {
+        return Err(Base64Error::InvalidLength);
+    }
+
+    // Process src[0..src_len-4]
+    let mut src_idx = 0;
+    let mut dst_idx = 0;
+    while src_idx + 4 < src_len {
+        let val: u32 = u32::from(from_char(src[src_idx + 0], decode_map)?) << 18
+            | u32::from(from_char(src[src_idx + 1], decode_map)?) << 12
+            | u32::from(from_char(src[src_idx + 2], decode_map)?) << 6
+            | u32::from(from_char(src[src_idx + 3], decode_map)?);
+
+        dst[dst_idx + 0] = ((val >> 16) & 0xFF) as u8;
+        dst[dst_idx + 1] = ((val >> 8) & 0xFF) as u8;
+        dst[dst_idx + 2] = (val & 0xFF) as u8;
+
+        src_idx += 4;
+        dst_idx += 3;
+    }
+
+    // Process last 4 bytes
+    if src[src_idx + 2] == b'=' {
+        // two padding char, [c c = =], convert to one dst byte
+        let val: u32 = u32::from(from_char(src[src_idx + 0], decode_map)?) << 18
+            | u32::from(from_char(src[src_idx + 1], decode_map)?) << 12;
+        dst[dst_idx + 0] = ((val >> 16) & 0xFF) as u8;
+        dst_idx += 1;
+    } else if src[src_idx + 3] == b'=' {
+        // one padding char, [c c c =], convert to two dst byte
+        let val: u32 = u32::from(from_char(src[src_idx + 0], decode_map)?) << 18
+            | u32::from(from_char(src[src_idx + 1], decode_map)?) << 12
+            | u32::from(from_char(src[src_idx + 2], decode_map)?) << 6;
+        dst[dst_idx + 0] = ((val >> 16) & 0xFF) as u8;
+        dst[dst_idx + 1] = ((val >> 8) & 0xFF) as u8;
+        dst_idx += 2;
+    } else {
+        // no padding char, [c c c c], convert to three dst byte
+        let val: u32 = u32::from(from_char(src[src_idx + 0], decode_map)?) << 18
+            | u32::from(from_char(src[src_idx + 1], decode_map)?) << 12
+            | u32::from(from_char(src[src_idx + 2], decode_map)?) << 6
+            | u32::from(from_char(src[src_idx + 3], decode_map)?);
+        dst[dst_idx + 0] = ((val >> 16) & 0xFF) as u8;
+        dst[dst_idx + 1] = ((val >> 8) & 0xFF) as u8;
+        dst[dst_idx + 2] = (val & 0xFF) as u8;
+        dst_idx += 3;
+    }
+
+    Ok(dst_idx)
+}
+
+pub fn decode_str(src: &str) -> Result<Vec<u8>, Base64Error> {
+    let mut dst = vec![0; decode_len(src.len())];
+    let dst_size = decode(&mut dst, src.as_bytes(), DECODE_STD_MAP)?;
+    println!("{}, {:?}", dst_size, dst);
+    dst.drain(dst_size..);
+    Ok(dst)
+}
+
 #[cfg(test)]
 mod tests_base64 {
     use super::*;
@@ -192,7 +315,7 @@ mod tests_base64 {
     }
 
     #[test]
-    fn test01() {
+    fn test_encode_str_01() {
         assert_eq!(encode_str(""), "".to_string());
         assert_eq!(encode_str("f"), "Zg==".to_string());
         assert_eq!(encode_str("fo"), "Zm8=".to_string());
@@ -203,7 +326,7 @@ mod tests_base64 {
     }
 
     #[test]
-    fn test02() {
+    fn test_encode_str_02() {
         assert_eq!(encode_str("sure."), "c3VyZS4=".to_string());
         assert_eq!(encode_str("sure"), "c3VyZQ==".to_string());
         assert_eq!(encode_str("sur"), "c3Vy".to_string());
@@ -215,7 +338,7 @@ mod tests_base64 {
     }
 
     #[test]
-    fn test03() {
+    fn test_encode_str_03() {
         assert_eq!(encode_str("hello"), "aGVsbG8=".to_string());
         assert_eq!(encode_bytes(b"hello"), b"aGVsbG8=");
     }
@@ -224,5 +347,28 @@ mod tests_base64 {
     fn test_decode_map_01() {
         let m = decode_map(ENCODE_STD);
         assert_eq!(m, DECODE_STD_MAP);
+    }
+
+    #[test]
+    fn test_decode_str_01() {
+        assert_eq!(decode_str(""), Err(Base64Error::InvalidLength));
+        assert_eq!(decode_str("Zg=="), Ok((b"f").to_vec()));
+        assert_eq!(decode_str("Zm8="), Ok((b"fo").to_vec()));
+        assert_eq!(decode_str("Zm9v"), Ok((b"foo").to_vec()));
+        assert_eq!(decode_str("Zm9vYg=="), Ok((b"foob").to_vec()));
+        assert_eq!(decode_str("Zm9vYmE="), Ok((b"fooba").to_vec()));
+        assert_eq!(decode_str("Zm9vYmFy"), Ok((b"foobar").to_vec()));
+    }
+
+    #[test]
+    fn test_decode_str_02() {
+        assert_eq!(decode_str("c3VyZS4="), Ok((b"sure.").to_vec()));
+        assert_eq!(decode_str("c3VyZQ=="), Ok((b"sure").to_vec()));
+        assert_eq!(decode_str("c3Vy"), Ok((b"sur").to_vec()));
+        assert_eq!(decode_str("c3U="), Ok((b"su").to_vec()));
+        assert_eq!(decode_str("bGVhc3VyZS4="), Ok((b"leasure.").to_vec()));
+        assert_eq!(decode_str("ZWFzdXJlLg=="), Ok((b"easure.").to_vec()));
+        assert_eq!(decode_str("YXN1cmUu"), Ok((b"asure.").to_vec()));
+        assert_eq!(decode_str("c3VyZS4="), Ok((b"sure.").to_vec()));
     }
 }
